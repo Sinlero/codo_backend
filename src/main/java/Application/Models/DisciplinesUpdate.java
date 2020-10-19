499a2f3b23796bae3ee2ee7f7d5ddb00191d6315
Goldfinch <goldfinch_p8a@bitbucket.org>
2020-10-19 19:49:20 +0900

    add Lombok

--- src/main/java/Application/Models/DisciplinesUpdate.java
+++ src/main/java/Application/Models/DisciplinesUpdate.java
@@ -1,7 +1,14 @@
 package Application.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class DisciplinesUpdate {
     private Long userId;
     private ArrayList<Long> disciplines;
