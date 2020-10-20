30569ac9797ea8f25ba590e058b553fc54805821
goldfinch_p8a <wanted1345@gmail.com>
2020-10-20 14:10:19 +0900

    add method to get all disciplines

--- src/main/java/Application/Servicies/DisciplineService.java
+++ src/main/java/Application/Servicies/DisciplineService.java
@@ -1,13 +1,21 @@
 package Application.Servicies;
 
+import Application.Entities.Discipline;
+import Application.Repositories.DisciplineRepository;
 import Application.Repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DisciplineService {
 
     @Autowired
-    StudentRepository studentRepository;
+    DisciplineRepository disciplineRepository;
+
+    public List<Discipline> getAllDisciplines(){
+        return (List<Discipline>) disciplineRepository.findAll();
+    }
 
 }
