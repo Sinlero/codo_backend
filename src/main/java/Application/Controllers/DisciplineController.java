e954c09a340848d9a0c5dd95b8e31ad119eb686e
goldfinch_p8a <wanted1345@gmail.com>
2020-10-20 14:09:48 +0900

    add mapping getAllDisciplines

--- src/main/java/Application/Controllers/DisciplineController.java
+++ src/main/java/Application/Controllers/DisciplineController.java
@@ -1,7 +1,20 @@
 package Application.Controllers;
 
+import Application.Servicies.DisciplineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/discipline")
 public class DisciplineController {
+
+    @Autowired
+    DisciplineService disciplineService;
+
+    @RequestMapping("/getAllDisciplines")
+    public ResponseEntity getAllDisciplines() {
+        return ResponseEntity.ok(disciplineService.getAllDisciplines());
+    }
 }
