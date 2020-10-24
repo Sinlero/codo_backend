f535e58e766f485c529bbd35b792569bcf6669d6
Goldfinch <goldfinch_p8a@bitbucket.org>
2020-10-24 19:28:25 +0900

    remove unnecessary imports

--- src/main/java/Application/Entities/Discipline.java
+++ src/main/java/Application/Entities/Discipline.java
@@ -1,15 +1,11 @@
 package Application.Entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.List;
 
 @Entity(name = "disciplines")
 @Data
