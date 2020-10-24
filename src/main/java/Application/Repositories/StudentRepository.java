792b43dc259d2c896b7ea48be9b129461ac67e5c
Goldfinch <goldfinch_p8a@bitbucket.org>
2020-10-24 19:13:07 +0900

    create StudentRepository

--- src/main/java/Application/Repositories/StudentRepository.java
+++ src/main/java/Application/Repositories/StudentRepository.java
@@ -1,13 +1,13 @@
 package Application.Repositories;
 
-import Application.Entities.Discipline;
 import Application.Entities.Student;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student,Long> {
 
+    Optional<Student> findStudentByLogin(String login);
 }
