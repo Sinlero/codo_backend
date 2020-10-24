66c8b7a8937e6af296dbd5a8f7dfbb47f5e5ae20
Goldfinch <goldfinch_p8a@bitbucket.org>
2020-10-24 19:12:20 +0900

    now extends User

--- src/main/java/Application/Entities/Teacher.java
+++ src/main/java/Application/Entities/Teacher.java
@@ -1,25 +1,13 @@
 package Application.Entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
 
 @Entity(name = "teachers")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Teacher {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long id;
-    private String login;
-    @JsonIgnore
-    private String password;
-    private String fio;
+public class Teacher extends User {
+
 }
