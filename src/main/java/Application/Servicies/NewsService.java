52b7eb0bf26151b4dac6de00257345549410f67a
Sinlero <wanted1345@gmail.com>
2020-10-25 14:10:08 +0900

    add transaction news to database

--- src/main/java/Application/Servicies/NewsService.java
+++ src/main/java/Application/Servicies/NewsService.java
@@ -1,5 +1,6 @@
 package Application.Servicies;
 
+import Application.Entities.News;
 import Application.Repositories.NewsRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Service
 public class NewsService {
@@ -19,7 +21,7 @@ public class NewsService {
         this.newsRepository = newsRepository;
     }
 
-    public ResponseEntity<String> upload(MultipartFile file) {
+    public ResponseEntity<String> upload(String head, String text, MultipartFile file) {
         if (file.isEmpty()) {
             return new ResponseEntity<>("Error", HttpStatus.NO_CONTENT);
         }
@@ -32,6 +34,11 @@ public class NewsService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        String date = LocalDateTime.now().getHour() + ":" + LocalDateTime.now().getMinute() + " " +
+                LocalDateTime.now().getDayOfMonth()+ "." + LocalDateTime.now().getMonth().getValue()+ "." +
+                LocalDateTime.now().getYear();
+        News news = new News(head, text, newFile.getAbsolutePath(), date);
+        newsRepository.save(news);
         return new ResponseEntity<>(newFile.getAbsolutePath(), HttpStatus.OK);
     }
 }
