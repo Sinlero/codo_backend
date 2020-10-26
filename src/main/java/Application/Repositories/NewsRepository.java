280e80568f6e0214b784b0ef0008fe4f758d0619
goldfinch_p8a <wanted1345@gmail.com>
2020-10-26 17:20:59 +0900

    add requests findById and findByImg

--- src/main/java/Application/Repositories/NewsRepository.java
+++ src/main/java/Application/Repositories/NewsRepository.java
@@ -4,6 +4,12 @@ import Application.Entities.News;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface NewsRepository extends CrudRepository<News, Long> {
+
+    Optional<News> findById(Long id);
+
+    Optional<News> findByImg(String img);
 }
