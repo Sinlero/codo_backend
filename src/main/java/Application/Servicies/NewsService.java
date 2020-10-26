9d9223a18ccd43a1f8fd27e0f126903ef2263ffc
goldfinch_p8a <wanted1345@gmail.com>
2020-10-26 17:21:45 +0900

    add upload news and getting image of news

--- src/main/java/Application/Servicies/NewsService.java
+++ src/main/java/Application/Servicies/NewsService.java
@@ -2,15 +2,15 @@ package Application.Servicies;
 
 import Application.Entities.News;
 import Application.Repositories.NewsRepository;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class NewsService {
@@ -25,7 +25,11 @@ public class NewsService {
         if (file.isEmpty()) {
             return new ResponseEntity<>("Error", HttpStatus.NO_CONTENT);
         }
-        File newFile = new File("/" + file.getOriginalFilename());
+        File newFile = new File(System.getProperty("user.dir") + "/NewsImages/");
+        if (!newFile.exists()) {
+            newFile.mkdirs();
+        }
+        newFile = new File(newFile, file.getOriginalFilename());
         try {
             FileOutputStream stream = new FileOutputStream(newFile);
             stream.write(file.getBytes());
@@ -35,10 +39,51 @@ public class NewsService {
             e.printStackTrace();
         }
         String date = LocalDateTime.now().getHour() + ":" + LocalDateTime.now().getMinute() + " " +
-                LocalDateTime.now().getDayOfMonth()+ "." + LocalDateTime.now().getMonth().getValue()+ "." +
+                LocalDateTime.now().getDayOfMonth() + "." + LocalDateTime.now().getMonth().getValue() + "." +
                 LocalDateTime.now().getYear();
         News news = new News(head, text, newFile.getAbsolutePath(), date);
         newsRepository.save(news);
-        return new ResponseEntity<>(newFile.getAbsolutePath(), HttpStatus.OK);
+        System.out.println(newFile);
+        Optional<News> newsId = newsRepository.findByImg(newFile.getAbsolutePath());
+        if (!newsId.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(newsId.get().getId().toString(), HttpStatus.OK);
+    }
+
+    public HttpEntity<byte[]> getImage(Long id) {
+        Optional<News> news = newsRepository.findById(id);
+        try {
+            if (!news.isPresent()) {
+                return new HttpEntity(HttpStatus.NOT_FOUND);
+            }
+            File image = new File(news.get().getImg());
+            return new HttpEntity<>(getBytes(image));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public byte[] getBytes(File file) throws IOException {
+        long length = file.length();
+        if (length > Integer.MAX_VALUE) {
+            throw new IOException("File is too large");
+        }
+        byte[] bytes = new byte[(int) length];
+        int offset = 0;
+        int numRead = 0;
+        InputStream is = new FileInputStream(file);
+        try {
+            while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+                offset += numRead;
+            }
+        } finally {
+            is.close();
+        }
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file " + file.getName());
+        }
+        return bytes;
     }
 }
