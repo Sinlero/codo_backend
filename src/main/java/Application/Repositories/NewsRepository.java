59631a995056019f09cdae8cc5aef55493339aa6
Sinlero <wanted1345@gmail.com>
2020-10-27 22:11:34 +0900

    remove unnecessary request, add request to get news in reverse order

--- src/main/java/Application/Repositories/NewsRepository.java
+++ src/main/java/Application/Repositories/NewsRepository.java
@@ -4,6 +4,7 @@ import Application.Entities.News;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,5 +12,5 @@ public interface NewsRepository extends CrudRepository<News, Long> {
 
     Optional<News> findById(Long id);
 
-    Optional<News> findByImg(String img);
+    List<News> findAllByOrderByIdDesc();
 }
