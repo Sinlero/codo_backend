9b634080b7873f375b7165f2fb8d159860bcbb23
Sinlero <wanted1345@gmail.com>
2020-11-04 17:08:14 +0900

    refactoring

--- src/main/java/Application/Repositories/NewsRepository.java
+++ src/main/java/Application/Repositories/NewsRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface NewsRepository extends CrudRepository<News, Long> {
 
-//    Optional<News> findById(Long id);
-
     List<News> findAllByOrderByIdDesc();
+
 }
