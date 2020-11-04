80cd87d6fa9ecb39e8fe593bf05305924bc4132a
Sinlero <wanted1345@gmail.com>
2020-11-04 15:42:49 +0900

    remove field text, add fields previewText and fullText

--- src/main/java/Application/Entities/News.java
+++ src/main/java/Application/Entities/News.java
@@ -2,27 +2,18 @@ package Application.Entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity(name = "news")
 @AllArgsConstructor
-@NoArgsConstructor
 @Data
-public class News {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String head;
-    private String text;
-    @OneToOne
-    private Image image;
-    private String date;
+public class News extends AbstractEvent {
 
-    public News(String head, String text, Image image, String date) {
+    public News(String head, String previewText, String fullText, Image image, String date) {
         this.head = head;
-        this.text = text;
+        this.previewText = previewText;
+        this.fullText = fullText;
         this.image = image;
         this.date = date;
     }
