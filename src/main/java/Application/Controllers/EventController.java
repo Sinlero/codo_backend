79e7888100fe0173ad3786d490d8e070a902893d
Sinlero <wanted1345@gmail.com>
2020-11-15 15:26:14 +0900

    add event controller

---
+++ src/main/java/Application/Controllers/EventController.java
@@ -0,0 +1,59 @@
+package Application.Controllers;
+
+import Application.Servicies.EventService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/event")
+public class EventController {
+
+    private EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @RequestMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam(required = false) MultipartFile file, @RequestParam String head,
+                                         @RequestParam String previewText, @RequestParam String fullText,
+                                         @RequestParam String date) {
+        return eventService.upload(file, head, previewText, fullText, date);
+    }
+
+    @RequestMapping("/{id}/delete")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        return eventService.deleteById(id);
+    }
+
+    @RequestMapping("/{id}/update")
+    public ResponseEntity<String> update(@PathVariable Long id, @RequestParam String head,
+                                         @RequestParam String previewText,
+                                         @RequestParam String fullText) {
+        return eventService.update(id, head, previewText, fullText);
+    }
+
+    @RequestMapping("/getAll")
+    public ResponseEntity getAll() {
+        return eventService.getAll();
+    }
+
+    @RequestMapping("/getActual")
+    public ResponseEntity getActual() {
+        return eventService.getActual();
+    }
+
+    @RequestMapping("/{id}")
+    public ResponseEntity getById(@PathVariable Long id) {
+        return eventService.getById(id);
+    }
+
+    @RequestMapping("/{id}/fullText")
+    public String getFullText(@PathVariable Long id){
+        return eventService.getFullTextById(id);
+    }
+}
