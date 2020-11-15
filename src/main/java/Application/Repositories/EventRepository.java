0f434182df15a8064b2be495201fbefa696951d0
Sinlero <wanted1345@gmail.com>
2020-11-15 15:26:26 +0900

    add event repository

---
+++ src/main/java/Application/Repositories/EventRepository.java
@@ -0,0 +1,18 @@
+package Application.Repositories;
+
+import Application.Entities.Event;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface EventRepository extends CrudRepository<Event, Long> {
+
+    // irrelevant events
+    // List<Event> findEventsByDateLess(LocalDate date);
+
+    // actual events
+    List<Event> findEventsByDateGreaterThanEqual(LocalDate date);
+}
