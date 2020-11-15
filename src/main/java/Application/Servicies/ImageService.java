71be708b126a623965f674921ec089c03357da0e
Sinlero <wanted1345@gmail.com>
2020-11-15 15:28:48 +0900

    add method to get image by id

--- src/main/java/Application/Servicies/ImageService.java
+++ src/main/java/Application/Servicies/ImageService.java
@@ -2,10 +2,13 @@ package Application.Servicies;
 
 import Application.Entities.Image;
 import Application.Repositories.ImageRepository;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.Optional;
 
 @Service
 public class ImageService {
@@ -28,6 +31,20 @@ public class ImageService {
             Image image = new Image(newFile.getAbsolutePath());
             imageRepository.save(image);
             return image.getId();
+        }
+    }
+
+    public HttpEntity<byte[]> getImage(Long id) {
+        Optional<Image> image = imageRepository.findById(id);
+        try {
+            if (!image.isPresent()) {
+                return new HttpEntity(HttpStatus.NOT_FOUND);
+            }
+            File imageFile = new File(image.get().getPath());
+            return new HttpEntity<>(FileService.getBytes(imageFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
