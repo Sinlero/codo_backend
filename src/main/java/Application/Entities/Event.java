2401b4d7440fd294ceb63968a78d200f6e0d0aa3
Sinlero <wanted1345@gmail.com>
2020-11-15 15:25:48 +0900

    add event entity

---
+++ src/main/java/Application/Entities/Event.java
@@ -0,0 +1,19 @@
+package Application.Entities;
+
+import lombok.AllArgsConstructor;
+
+import javax.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity(name = "events")
+@AllArgsConstructor
+public class Event extends AbstractEvent {
+
+    public Event(String head, String previewText, String fullText, Image image, LocalDate date) {
+        this.head = head;
+        this.previewText = previewText;
+        this.fullText = fullText;
+        this.image = image;
+        this.date = date;
+    }
+}
