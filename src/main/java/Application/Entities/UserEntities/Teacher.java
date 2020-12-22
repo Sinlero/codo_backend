6d94a8bf9469a3fddbe25846c990739efff32f3e
goldfinch_p8a <wanted1345@gmail.com>
2020-12-22 14:42:19 +0900

    add annotation NoArgsConstructor

--- src/main/java/Application/Entities/UserEntities/Teacher.java
+++ src/main/java/Application/Entities/UserEntities/Teacher.java
@@ -1,13 +1,13 @@
 package Application.Entities.UserEntities;
 
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 
-
 @Entity(name = "teachers")
 @Data
+@NoArgsConstructor
 public class Teacher extends AbstractUser {
 
 }
