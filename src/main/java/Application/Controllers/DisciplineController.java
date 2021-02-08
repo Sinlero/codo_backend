5dde617c04a6ea4c370b47b9e357e2777f3041f0
goldfinch_p8a <wanted1345@gmail.com>
2021-02-08 09:04:35 +0900

    create addDiscipline method

--- src/main/java/Application/Controllers/DisciplineController.java
+++ src/main/java/Application/Controllers/DisciplineController.java
@@ -3,7 +3,11 @@ package Application.Controllers;
 import Application.Servicies.DisciplineService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/discipline")
@@ -18,5 +22,11 @@ public class DisciplineController {
     @RequestMapping("/getAll")
     public ResponseEntity getAll() {
         return ResponseEntity.ok(disciplineService.getAll());
+    }
+
+    @RequestMapping("/add")
+    public ResponseEntity<String> addDiscipline(@RequestParam String name, @RequestParam BigDecimal cost,
+                                        @RequestParam ArrayList<Long> teacherIDs, @RequestParam String colorCode) {
+        return disciplineService.addDiscipline(name, cost, teacherIDs, colorCode);
     }
 }
--- src/main/java/Application/Servicies/DisciplineService.java
+++ src/main/java/Application/Servicies/DisciplineService.java
@@ -1,22 +1,46 @@
 package Application.Servicies;
 
+import Application.Data.Repositories.TeacherRepository;
 import Application.Entities.Discipline;
 import Application.Data.Repositories.DisciplineRepository;
+import Application.Entities.UserEntities.Teacher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DisciplineService {
 
     DisciplineRepository disciplineRepository;
+    TeacherRepository teacherRepository;
 
-    public DisciplineService(DisciplineRepository disciplineRepository) {
+    public DisciplineService(DisciplineRepository disciplineRepository, TeacherRepository teacherRepository) {
         this.disciplineRepository = disciplineRepository;
+        this.teacherRepository = teacherRepository;
     }
 
     public List<Discipline> getAll(){
         return (List<Discipline>) disciplineRepository.findAll();
     }
 
+    public ResponseEntity<String> addDiscipline(@RequestParam String name, @RequestParam BigDecimal cost,
+                                        @RequestParam ArrayList<Long> teacherIDs, @RequestParam String colorCode) {
+        ArrayList<Teacher> teachers = new ArrayList<>();
+        for (Long teacherID : teacherIDs) {
+            Optional<Teacher> teacher = teacherRepository.findById(teacherID);
+            if(!teacher.isPresent()) {
+                return new ResponseEntity<>(String.format("Teacher with id %s not found", teacherID), HttpStatus.NOT_FOUND);
+            }
+            teachers.add(teacher.get());
+        }
+        Discipline discipline = new Discipline(name, cost, teachers, colorCode);
+        disciplineRepository.save(discipline);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
 }
