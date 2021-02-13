683fc9df557a580b255e920a555aea994e406914
Sinlero <wanted1345@gmail.com>
2021-02-13 14:12:08 +0900

    create teacher controller with endpoints: add teacher, get list of all teachers

---
+++ src/main/java/Application/Controllers/UserControllers/TeacherController.java
@@ -0,0 +1,32 @@
+package Application.Controllers.UserControllers;
+
+import Application.Entities.UserEntities.Teacher;
+import Application.Servicies.TeacherService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teacher")
+public class TeacherController {
+
+    private TeacherService teacherService;
+
+    public TeacherController(TeacherService teacherService){
+        this.teacherService = teacherService;
+    }
+
+    @RequestMapping("/getAll")
+    public List<Teacher> getAll() {
+        return teacherService.getAll();
+    }
+
+    @RequestMapping("/add")
+    public ResponseEntity<String> add(@RequestBody Teacher teacher) {
+        System.out.println(teacher.getPhoneNumber());
+        return teacherService.add(teacher);
+    }
+}
