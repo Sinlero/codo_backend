71a3c020f8d39bf476ebf3c51d70cff3768ccb3f
Sinlero <wanted1345@gmail.com>
2021-02-28 13:17:13 +0900

    add delete teacher method and some refactoring add method

--- src/main/java/Application/Servicies/TeacherService.java
+++ src/main/java/Application/Servicies/TeacherService.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TeacherService {
@@ -23,6 +24,15 @@ public class TeacherService {
 
     public ResponseEntity<String> add(Teacher teacher) {
         teacherRepository.save(teacher);
-        return new ResponseEntity<>("Success", HttpStatus.OK);
+        return new ResponseEntity<>("Teacher added", HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> delete(Long id) {
+        Optional<Teacher> teacher = teacherRepository.findById(id);
+        if(!teacher.isPresent()) {
+            return new ResponseEntity<>("Teacher with this id not found", HttpStatus.NOT_FOUND);
+        }
+        teacherRepository.deleteById(id);
+        return new ResponseEntity<>("Teacher deleted", HttpStatus.OK);
     }
 }
