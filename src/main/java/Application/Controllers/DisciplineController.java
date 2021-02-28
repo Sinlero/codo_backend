72b80bd890b1704c94c569d8ccde47f1f88a11fd
Sinlero <wanted1345@gmail.com>
2021-02-28 13:32:42 +0900

    set teacherIDs as required false; add delete method

--- src/main/java/Application/Controllers/DisciplineController.java
+++ src/main/java/Application/Controllers/DisciplineController.java
@@ -2,6 +2,7 @@ package Application.Controllers;
 
 import Application.Servicies.DisciplineService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,13 @@ public class DisciplineController {
 
     @RequestMapping("/add")
     public ResponseEntity<String> addDiscipline(@RequestParam String name, @RequestParam BigDecimal cost,
-                                        @RequestParam ArrayList<Long> teacherIDs, @RequestParam(required = false) String colorCode) {
+                                                @RequestParam(required = false) ArrayList<Long> teacherIDs,
+                                                @RequestParam(required = false) String colorCode) {
         return disciplineService.addDiscipline(name, cost, teacherIDs, colorCode);
+    }
+
+    @RequestMapping("/{id}/delete")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        return disciplineService.delete(id);
     }
 }
