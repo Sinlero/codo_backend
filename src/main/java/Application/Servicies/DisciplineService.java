dba5736624b17703d28a7f75748c475f3c39b842
Sinlero <wanted1345@gmail.com>
2021-02-28 13:33:56 +0900

    add delete method, check on null/empty teacherIDs, some refactoring

--- src/main/java/Application/Servicies/DisciplineService.java
+++ src/main/java/Application/Servicies/DisciplineService.java
@@ -7,6 +7,7 @@ import Application.Entities.UserEntities.Teacher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -25,29 +26,45 @@ public class DisciplineService {
         this.teacherRepository = teacherRepository;
     }
 
-    public List<Discipline> getAll(){
+    public List<Discipline> getAll() {
         return (List<Discipline>) disciplineRepository.findAll();
     }
 
+    @Transactional
     public ResponseEntity<String> addDiscipline(String name, BigDecimal cost, ArrayList<Long> teacherIDs,
                                                 String colorCode) {
-        ArrayList<Teacher> teachers = new ArrayList<>();
         if (colorCode == null || colorCode.isEmpty()) {
             colorCode = generateColor();
         }
+        if (teacherIDs == null || teacherIDs.isEmpty()) {
+            Discipline discipline = new Discipline(name, cost, colorCode);
+            disciplineRepository.save(discipline);
+            return new ResponseEntity<>("Discipline added", HttpStatus.OK);
+        }
+        ArrayList<Teacher> teachers = new ArrayList<>();
         for (Long teacherID : teacherIDs) {
             Optional<Teacher> teacher = teacherRepository.findById(teacherID);
-            if(!teacher.isPresent()) {
+            if (!teacher.isPresent()) {
                 return new ResponseEntity<>(String.format("Teacher with id %s not found", teacherID), HttpStatus.NOT_FOUND);
             }
             teachers.add(teacher.get());
         }
         Discipline discipline = new Discipline(name, cost, teachers, colorCode);
         disciplineRepository.save(discipline);
-        return new ResponseEntity<>("Success", HttpStatus.OK);
+        return new ResponseEntity<>("Discipline added", HttpStatus.OK);
     }
 
-    public String generateColor(){
+    @Transactional
+    public ResponseEntity<String> delete(Long id) {
+        Optional<Discipline> discipline = disciplineRepository.findById(id);
+        if (!discipline.isPresent()) {
+            return new ResponseEntity<>("Discipline with this id not found", HttpStatus.NOT_FOUND);
+        }
+        disciplineRepository.deleteById(id);
+        return new ResponseEntity<>("Discipline deleted", HttpStatus.OK);
+    }
+
+    public String generateColor() {
         StringBuilder color = new StringBuilder("#");
         String[] letters = "0123456789abcdef".split("");
         Random random = new Random();
