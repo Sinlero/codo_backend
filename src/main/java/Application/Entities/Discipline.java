b2ec98c8cf1ea36946eb84568dc446c5175fe8f7
Sinlero <wanted1345@gmail.com>
2021-02-28 13:31:31 +0900

    fix looping entity

--- src/main/java/Application/Entities/Discipline.java
+++ src/main/java/Application/Entities/Discipline.java
@@ -2,6 +2,7 @@ package Application.Entities;
 
 import Application.Entities.UserEntities.Student;
 import Application.Entities.UserEntities.Teacher;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,8 +22,10 @@ public class Discipline {
     private String name;
     private BigDecimal cost;
     @ManyToMany(mappedBy = "disciplines")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<Teacher> teacher;
     @ManyToMany(mappedBy = "disciplines")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<Student> student ;
     private String colorCode;
 
