ac289d9a90f6dbb1b50835325c98c5b9f79065a8
Sinlero <wanted1345@gmail.com>
2021-03-08 19:21:40 +0900

    create lesson controller with add mapping

---
+++ src/main/java/Application/Controllers/LessonController.java
@@ -0,0 +1,25 @@
+package Application.Controllers;
+
+import Application.Entities.Lesson;
+import Application.Servicies.LessonService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lesson")
+public class LessonController {
+
+    private LessonService lessonService;
+
+    public LessonController(LessonService lessonService) {
+        this.lessonService = lessonService;
+    }
+
+    @RequestMapping("/add")
+    public ResponseEntity<String> add(@RequestBody Lesson lesson) {
+        return lessonService.add(lesson);
+    }
+
+}
