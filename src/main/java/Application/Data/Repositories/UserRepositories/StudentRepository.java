3a0c76eacff36afd3ac937b887e2586c4914adb4
goldfinch_p8a <wanted1345@gmail.com>
2021-03-15 13:43:34 +0900

    add query get all students order by id ASC

--- src/main/java/Application/Data/Repositories/UserRepositories/StudentRepository.java
+++ src/main/java/Application/Data/Repositories/UserRepositories/StudentRepository.java
@@ -4,10 +4,13 @@ import Application.Entities.UserEntities.Student;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student,Long> {
 
     Optional<Student> findStudentByLogin(String login);
+
+    List<Student> findAllByOrderByIdAsc();
 }
