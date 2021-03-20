a21b212ef4a0c33952323ba6177d042a3d593312
Sinlero <wanted1345@gmail.com>
2021-03-20 13:33:40 +0900

    change generation on HSV colors

--- src/main/java/Application/Configuration/ApplicationConfig.java
+++ src/main/java/Application/Configuration/ApplicationConfig.java
@@ -2,7 +2,7 @@ package Application.Configuration;
 
 
 import Application.Utils.Color.ColorCodeGenerator;
-import Application.Utils.Color.RandomColorCodeGenerator;
+import Application.Utils.Color.HsvColorGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +11,6 @@ public class ApplicationConfig {
 
     @Bean
     public ColorCodeGenerator colorCodeGenerator() {
-        return new RandomColorCodeGenerator();
+        return new HsvColorGenerator();
     }
 }
