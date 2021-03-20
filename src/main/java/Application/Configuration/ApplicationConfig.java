adf777a9ec8855369b420d93f0e6c844529775c8
Sinlero <wanted1345@gmail.com>
2021-03-20 12:38:22 +0900

    add application configuration with color generator bean

---
+++ src/main/java/Application/Configuration/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package Application.Configuration;
+
+
+import Application.Utils.Color.ColorCodeGenerator;
+import Application.Utils.Color.RandomColorCodeGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public ColorCodeGenerator colorCodeGenerator() {
+        return new RandomColorCodeGenerator();
+    }
+}
