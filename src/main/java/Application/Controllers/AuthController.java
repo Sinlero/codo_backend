4b698d9924056277b583044bdf8076d2e2252c1a
goldfinch_p8a <wanted1345@gmail.com>
2021-04-12 18:06:14 +0900

    add logger

--- src/main/java/Application/Controllers/AuthController.java
+++ src/main/java/Application/Controllers/AuthController.java
@@ -1,18 +1,29 @@
 package Application.Controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/login")
     public ResponseEntity auth() {
         try {
-            return new ResponseEntity(HttpStatus.OK);
+            SecurityContext context = SecurityContextHolder.getContext();
+            Authentication authentication = context.getAuthentication();
+            logger.info(authentication.getName() + " login with role " + authentication.getAuthorities());
+            return new ResponseEntity(authentication.getAuthorities(), HttpStatus.OK);
         } catch (BadCredentialsException ex) {
             throw new BadCredentialsException("wrong", ex);
         }
