83e6fa49744a59f408f59e06f65fabb9f8ebfd15
goldfinch_p8a <wanted1345@gmail.com>
2021-04-12 18:06:53 +0900

    add cookie sameSite "None"

--- src/main/java/Application/Configuration/ApplicationConfig.java
+++ src/main/java/Application/Configuration/ApplicationConfig.java
@@ -3,6 +3,10 @@ package Application.Configuration;
 
 import Application.Utils.Color.ColorCodeGenerator;
 import Application.Utils.Color.HsvColorGenerator;
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +16,17 @@ public class ApplicationConfig {
     @Bean
     public ColorCodeGenerator colorCodeGenerator() {
         return new HsvColorGenerator();
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        return new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                Rfc6265CookieProcessor rfc6265Processor = new Rfc6265CookieProcessor();
+                rfc6265Processor.setSameSiteCookies("None");
+                context.setCookieProcessor(rfc6265Processor);
+            }
+        };
     }
 }
