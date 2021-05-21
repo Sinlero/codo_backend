d473acc57e7e9d14f6ca58afd51bb216380ff312
goldfinch_p8a <wanted1345@gmail.com>
2021-05-21 15:48:02 +0900

    add queries findAllByDay and findAllByCourse

--- src/main/java/Application/Data/Repositories/LessonRepository.java
+++ src/main/java/Application/Data/Repositories/LessonRepository.java
@@ -1,9 +1,18 @@
 package Application.Data.Repositories;
 
+import Application.Entities.Course;
 import Application.Entities.Lesson;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface LessonRepository extends CrudRepository<Lesson, Long> {
+
+    Optional<List<Lesson>> findAllByDate(LocalDate date);
+
+    Optional<List<Lesson>> findAllByCourse(Course courseId);
 }
