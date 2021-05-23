4e8554d9d20cc1bfb639f08b2c45ef15c6943a51
Sinlero <wanted1345@gmail.com>
2021-05-23 14:34:51 +0900

    add method to add journal (without balance control)

---
+++ src/main/java/Application/Services/JournalService.java
@@ -0,0 +1,51 @@
+package Application.Services;
+
+import Application.Data.DTO.Journal.JournalUpdate;
+import Application.Data.DTO.Journal.StudentInfo;
+import Application.Data.Repositories.JournalRepository;
+import Application.Data.Repositories.LessonRepository;
+import Application.Data.Repositories.UserRepositories.StudentRepository;
+import Application.Entities.Journal;
+import Application.Entities.Lesson;
+import Application.Entities.UserEntities.Student;
+import Application.Utils.Response.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JournalService {
+
+    private LessonRepository lessonRepository;
+    private StudentRepository studentRepository;
+    private JournalRepository journalRepository;
+
+    public JournalService(LessonRepository lessonRepository, StudentRepository studentRepository, JournalRepository journalRepository) {
+        this.lessonRepository = lessonRepository;
+        this.studentRepository = studentRepository;
+        this.journalRepository = journalRepository;
+    }
+
+    public ResponseEntity<String> add(JournalUpdate journalUpdate) {
+        Optional<Lesson> lesson = lessonRepository.findById(journalUpdate.getLessonId());
+        if (!lesson.isPresent()) {
+            return ResponseUtil.notFoundId("Lesson");
+        }
+        List<Journal> journal = new ArrayList<>();
+        for (StudentInfo studentInfo : journalUpdate.getJournalList()) {
+            Optional<Student> student = studentRepository.findById(studentInfo.getStudentId());
+            if (!student.isPresent()) {
+                return ResponseUtil.notFoundId("Student");
+            }
+            journal.add(new Journal(student.get(), lesson.get(), studentInfo.getPresence(), studentInfo.getMark()));
+        }
+        for (Journal saveJournal : journal) {
+            journalRepository.save(saveJournal);
+        }
+        return new ResponseEntity<>("Journal added", HttpStatus.OK);
+    }
+}
