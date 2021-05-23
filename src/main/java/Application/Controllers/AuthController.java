4ef9c9b0e74097821d296f018166a904db8863a8
goldfinch_p8a <wanted1345@gmail.com>
2021-05-24 08:37:27 +0900

    add DI with constructor

--- src/main/java/Application/Controllers/AuthController.java
+++ src/main/java/Application/Controllers/AuthController.java
@@ -5,7 +5,6 @@ import Application.Data.Repositories.UserRepositories.StudentRepository;
 import Application.Data.Repositories.UserRepositories.TeacherRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -19,16 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
-    @Autowired
     private AdministratorRepository administratorRepository;
-
-    @Autowired
     private StudentRepository studentRepository;
-
-    @Autowired
     private TeacherRepository teacherRepository;
-
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public AuthController(AdministratorRepository administratorRepository, TeacherRepository teacherRepository,
+                          StudentRepository studentRepository) {
+        this.administratorRepository = administratorRepository;
+        this.teacherRepository = teacherRepository;
+        this.studentRepository = studentRepository;
+    }
 
     @RequestMapping("/login")
     public ResponseEntity auth() {
