1781fe46738050ee20a09a538f4b5af794e56ad8
Sinlero <wanted1345@gmail.com>
2021-05-23 15:16:16 +0900

    add query find journal by lesson

--- src/main/java/Application/Data/Repositories/JournalRepository.java
+++ src/main/java/Application/Data/Repositories/JournalRepository.java
@@ -1,10 +1,15 @@
 package Application.Data.Repositories;
 
 import Application.Entities.Journal;
+import Application.Entities.Lesson;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JournalRepository extends CrudRepository<Journal, Long> {
 
+    Optional<List<Journal>> findAllByLesson(Lesson lesson);
 }
